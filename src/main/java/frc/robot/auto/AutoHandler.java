e1ecdd03d7b5a285b1185876edac2c6000ff4704
Dinoyan Ganeshalingam <dinoyan.g98@gmail.com>
2020-01-01 14:11:11 -0500

    auto distance builder

--- src/main/java/frc/robot/auto/AutoHandler.java
+++ src/main/java/frc/robot/auto/AutoHandler.java
@@ -16,27 +16,43 @@ import frc.robot.util.CyberPID;
 public class AutoHandler {
     private Drivetrain drive;
 
+    // pid controllers for subsystems
     private CyberPID pidController = new CyberPID();
     private CyberPID turnPID = new CyberPID();
     private CyberPID shooterPID = new CyberPID();
 
+    // auto modes
     private static final byte TEST_STATE_MACHINE = 1;
     private static final byte TEST_STATE_MACHINE_V2 = 2;
 
+    // auto actions
     private static final byte DRIVE_FORWARD = 1;
     private static final byte DRIVE_BACKWARD = 2;
     private static final byte WAIT_STATE = 3;
     private static final byte SHOOT_STATE = 4;
     private static final byte TURN_STATE = 5;
 
+    private byte mMode;
+    private int mCounter = 0;
+
     private byte currentState;
     private byte currentStateIndex = 0;
+    // auto mode actions container
     private byte[] nextStateArray = new byte[255];
 
+    // auto timer
     private Timer mTimer = new Timer();
     private Boolean mStop = false;
 
     private static AutoHandler mInstance = null;
+
+    // distance container
+    private double[] TEST_STATE_1_DISTANCE = new double[2];
+
+    private void buildDistances() {
+      TEST_STATE_1_DISTANCE[0] = 254.9;
+      TEST_STATE_1_DISTANCE[1] = 0.0;
+    }
 
     public static AutoHandler getInstance() {
       if (mInstance == null) {
@@ -50,10 +66,11 @@ public class AutoHandler {
       currentStateIndex = 0;
       setCurrentState(WAIT_STATE);
       setNextStateArray(selection);
-      
+    
+      buildDistances();
       mTimer.start();
     }
- 
+
     public void setNextStateArray(byte mode) {
       byte stateCounter = 0;
       if (TEST_STATE_MACHINE == mode) {
@@ -74,14 +91,23 @@ public class AutoHandler {
     }
 
     public void autonomousPeriodHandler() {
+      double distance = 0.0;
+
       if (currentState == DRIVE_FORWARD) {
-        this.driveToDistance(3);
+        
+        switch (mMode) {
+          case TEST_STATE_MACHINE:
+              distance = TEST_STATE_1_DISTANCE[mCounter];
+              break;
+      }
+
+        this.driveToDistance(distance);
       } else if (currentState == WAIT_STATE) {
-        // do nothing
+          // do nothing
       } else if (currentState == SHOOT_STATE) {
           this.shoot(3);
       } else if (currentState == TURN_STATE) {
-        this.turn(90);
+          this.turn(90);
       }
     }
 
@@ -95,7 +121,6 @@ public class AutoHandler {
     private void driveToDistance(double distance) {
       pidController.setSetpoint(distance);
       Boolean cond = infLoopChecker();
-      
       boolean onTarget = pidController.onTarget(this.drive.getLeftEncoderDis());
       
       if (!cond) {
@@ -111,16 +136,12 @@ public class AutoHandler {
         }
       }
       pidController.reset();
-    }
-
-    private void shoot(double speed) {
-      this.shooterPID.setSetpoint(speed);
+      this.mCounter++;
     }
 
     private void turn(double angle) {
       turnPID.setSetpoint(angle);
       boolean cond = infLoopChecker();
-
       boolean onTarget = turnPID.onTarget(this.drive.getAngle());
 
       if (!cond) {
@@ -135,6 +156,10 @@ public class AutoHandler {
             setCurrentState(nextStateArray[currentStateIndex]);
         }
       }
-      pidController.reset();
+      turnPID.reset();
+    }
+
+    private void shoot(double speed) {
+      this.shooterPID.setSetpoint(speed);
     }
 }
